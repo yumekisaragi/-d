b68deeb98a4c2a89f329028c4089c5e50819fe6f
yumekisaragi <towfew191@gmail.com>
2025-11-21 17:06:15 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -1,14 +1,14 @@
 // owner.js
 module.exports = {
   name: "owner",
-  description: "Show bot owner info. Usage: +owner",
+  description: "Show bot owner info. Usage: !owner",
   execute(api, event, args) {
     const ownerInfo = `
 👑 BOT OWNER 👑
-Name: Haraira Sajib 💋💦
-FB: https://www.facebook.com/ewrsajib77
+Name:𝐀𝐫𝐢𝐲𝐚𝐧 𝐀𝐡𝐚𝐦𝐞𝐝
+FB: https://www.facebook.com/share/16N24wYssU/
 GitHub: ** 😜💋💦
-Prefix: +
+Prefix: !
 `;
     api.sendMessage(ownerInfo, event.threadID, event.messageID);
   }
